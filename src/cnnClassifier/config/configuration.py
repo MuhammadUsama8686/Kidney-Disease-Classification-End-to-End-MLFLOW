b71a9059c95c2a298f248b8ea4a74f8871c87469
Muhammad Usama <muhammadusama8686@gmail.com>
2024-09-25 06:58:45 +0500

    Evaluation MLFlow Pipiline DOne

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import  DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from cnnClassifier.utils.common import read_yaml, create_directories, save_json
+from cnnClassifier.entity.config_entity import  DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig, EvaluationConfig
 import tensorflow as tf
 import os
 
@@ -74,3 +74,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/CT-KIDNEY-DATASET-Normal-Tumor",
+            mlflow_uri="https://dagshub.com/MuhammadUsama8686/Kidney-Disease-Classification-End-to-End-MLFLOW.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
